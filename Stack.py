feff4c238f7d3658444be605b0832e693d90dced
Jack Jones <44882194+jonesjacklewis@users.noreply.github.com>
2020-01-15 23:57:38 +0000

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 class Stack: #Creates a Class called Stack
     def __init__(self):#ran when an object is initialised
         self.stack=[]#creates a list which will be used to model a stack
