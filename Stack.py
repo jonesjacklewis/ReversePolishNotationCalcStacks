17ff10dab5ea95ef91f4bfaf9b7f4e408fa2f43b
Jack Jones <44882194+jonesjacklewis@users.noreply.github.com>
2020-02-04 21:18:59 +0000

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
-class Stack: #Creates a Class called Stack
-    def __init__(self):#ran when an object is initialised
-        self.stack=[]#creates a list which will be used to model a stack
+class Stack:    # Creates a Class called Stack
+    def __init__(self):  # ran when an object is initialised
+        self.stack = []  # creates a list which will be used to model a stack
 
-    def push(self,elem):
-        self.stack.append(elem)#pushes elem to the top of the stack
+    def push(self, elem):
+        self.stack.append(elem)  # pushes elem to the top of the stack
 
     def pop(self):
-        if len(self.stack)==0:
+        if len(self.stack) == 0:
             return None
         else:
-            return self.stack.pop()#returns and removes item at top of the stack
+            return self.stack.pop()  # returns and removes item at top of the stack
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
